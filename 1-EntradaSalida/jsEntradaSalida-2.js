b7b9d3de6c7b29aa16461e11afc2fe6954ba1b4a
NicolasGimenez <36281564+NicolasGimenez@users.noreply.github.com>
2018-02-08 20:39:55 -0300

    ejercicio 1y2
    alert,prompt,variables,valores literales ,indefinidos

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,21 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+		//defino una variable
+	var nombre;
+	//alert(nombre); asi me muestra undefine significa vacio que no tiene datos
+
+	nombre=prompt("ingrese su nombre","natalia natalia");// prompt me sirve para cargarle un dato a la variable pero a travaes del teclado
+	alert(nombre);
+
+
+
+	/*
+	nombre="Nicolas";asigno valor a variable
+	alert(nombre);muestro el valor de la variable
+	alert("nombre");
+	*/
+		// var person = prompt("Please enter your name", "Harry Potter");
+
 }
 
