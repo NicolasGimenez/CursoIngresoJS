0c3b427bb1bc79a8f897fe044650247f8a844d37
NicolasGimenez <36281564+NicolasGimenez@users.noreply.github.com>
2018-02-08 21:34:43 -0300

    act 3, 4 y 5
    id ,document entrada y salida

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var nombre;
+	nombre=document.getElementById('elNombre').value;//metodo de entrada de dato con el id y con el document tambien el igual depende de que lado ver en el anterior ej
+	alert(nombre);
+}
+
+
