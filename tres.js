0e08ade2bc4e7f5a06d4e945e2b7fa4b88c24516
NicolasGimenez <36281564+NicolasGimenez@users.noreply.github.com>
2018-02-09 21:10:28 -0300

    parciales echos

---
+++ tres.js
@@ -0,0 +1,17 @@
+function Mostrar()
+{
+
+	var largo;
+	var ancho;
+	var perimetro;
+	var alambre;
+
+	largo=document.getElementById('alrgo').value;
+	ancho=document.getElementById('ancho').value;
+
+	perimetro= largo*2 + ancho*2;
+	alambre=perimetro*3;
+
+	alert(alambre);
+
+}
---
+++ uno.js
@@ -0,0 +1,14 @@
+
+function Mostrar()
+{
+	var num1;
+	var resultado;
+
+
+	num1=document.getElementById('laBase').value;
+
+	resultado=num1*4;
+
+	alert("el perimetro es  "+resultado);
+	
+}
