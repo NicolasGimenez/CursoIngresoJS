3a165123d0e725096c65c42550b9bef387bae79a
NicolasGimenez <36281564+NicolasGimenez@users.noreply.github.com>
2018-02-15 22:13:54 -0300

    act 10 del if de otra manera

--- jsInstruccionIF (10).js
+++ jsInstruccionIF (10).js
@@ -2,9 +2,28 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	var nota;
-	nota=Math.floor(Math.random()*11);//si le pondria un 10 y un mas uno seria lo mismo pero no arrancaria del 0 
+	nota=Math.floor(Math.random()*10)+1;//si le pondria un 10 y un mas uno seria lo mismo pero no arrancaria del 0 
 
 		alert(nota);
+
+		if(nota>8)
+		{
+			alert("EXELENTE");
+		}
+		else
+		{
+			if(nota>3)
+			{
+				alert("APROBO");
+			}
+			else
+			{
+				alert("Vamos la proxima se puede");
+			}
+		}
+		
+			
+		/*
 
 	if (nota==9 || nota== 10)
 	{
@@ -17,6 +36,6 @@ function Mostrar()
 	if (nota<4)
 	{
 		alert("Vamos,la proxima se puede");
-	}
+	}*/
 
 }//FIN DE LA FUNCIÓN
