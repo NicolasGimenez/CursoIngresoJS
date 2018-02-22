fd276c6aff7bfb9c070d6c88358793a71a7ac60d
NicolasGimenez <36281564+NicolasGimenez@users.noreply.github.com>
2018-02-22 17:07:30 -0300

    act 6 switch terminada

--- jsInstruccionSwitch(6).js
+++ jsInstruccionSwitch(6).js
@@ -22,18 +22,24 @@ var laHora = document.getElementById('hora').value;
 		case "19":
 				 alert("Es de tarde");
 				 break;
-		case "":
-		case "":
-		case "":
-		case "":
-		case "":
-		case "":
-		case "":
-		case "":
-		case "":
-		case "":
-		case "":
-		case "":		 	     
+		case "20":
+		case "21":
+		case "22":
+		case "23":
+		case "24":
+		case "0":
+		case "1":
+		case "2":
+		case "3":
+		case "4":
+		case "5":
+		case "6":
+				 alert("Es de noche");
+				 break;
+		default:
+				alert("la hora no existe");
+				break;
+						 	     
 	}
 
 
